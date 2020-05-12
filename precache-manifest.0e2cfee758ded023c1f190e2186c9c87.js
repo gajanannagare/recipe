f6115b0d670663a44fcaf488cfc8ad6e9d111dce
Kailash <kailash.thale@gmail.com>
2020-05-12 14:40:58 +0530

    Updates

---
+++ precache-manifest.0e2cfee758ded023c1f190e2186c9c87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2d4c9d6e5e6b5a7a4e12eed7d530de8",
+    "url": "/recipe/index.html"
+  },
+  {
+    "revision": "4adfaeece70b4a900ba7",
+    "url": "/recipe/static/css/main.261a7bd8.chunk.css"
+  },
+  {
+    "revision": "cecf5586bf45c00ad258",
+    "url": "/recipe/static/js/2.f055bdba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipe/static/js/2.f055bdba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4adfaeece70b4a900ba7",
+    "url": "/recipe/static/js/main.7951b7a8.chunk.js"
+  },
+  {
+    "revision": "d08bfdff648e4ed2c176",
+    "url": "/recipe/static/js/runtime-main.c06e16f5.js"
+  }
+]);
